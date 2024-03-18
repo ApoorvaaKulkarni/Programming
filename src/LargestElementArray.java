9f53c50982294bdee5e08a2afc2b9388760e6e96
SHEETALAPOORVA <apoorvask111@gmai.com>
2024-03-18 23:30:09 +0530

    renamed the file as LargestElementArray

--- src/LargestElementArray.java
+++ src/LargestElementArray.java
@@ -1,20 +1,24 @@
 import java.util.Scanner;
 
-public class LargestOfthreeNumbers {
+public class LargestElementArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("\n Enter size");
-        int n = sc.nextInt();
+
+        int n = sc.nextInt(); // n = 3
         int[] array = new int[n];
+
         System.out.println("\n Enter array elements");
         for(int i =0;i < n;i++){
             array[i] = sc.nextInt();
         }
+
         System.out.println("\n Entered elements");
         for(int i =0; i< n;i++){
             System.out.print(array[i] + " ");
 
         }
+
         int temp = 0;
         for(int i =0; i < n ;i++){
             temp = array[0];
