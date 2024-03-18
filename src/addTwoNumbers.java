269696b241bbfe5754dbafa6e8f965953df89790
SHEETALAPOORVA <apoorvask111@gmai.com>
2024-03-19 00:06:44 +0530

    trying to add this file

---
+++ src/addTwoNumbers.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class addTwoNumbers {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        System.out.println(a + b);
+    }
+}
